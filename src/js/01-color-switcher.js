510564c9c66af67b5e8a6df48c87806e5d87895e
merry <k.maria.m.24.05@gmail.com>
2023-04-25 19:00:16 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+const intervalDelay = 1000;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+const intervalCallback = () => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+    btnStart.disabled = true;
+};
+
+const onStartBtnClick = () => {
+    timerId = setInterval(intervalCallback, intervalDelay);
+};
+
+btnStart.addEventListener('click', onStartBtnClick);
+
+const onStopBtnClick = () => {
+    btnStart.disabled = false;
+    clearInterval(timerId);
+};
+
+btnStop.addEventListener('click', onStopBtnClick);
 
